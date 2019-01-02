ff324a53f38e50419e067d47c88d886180c42de1
Jordi Escudero Subirana <jordi.escudero@gmail.com>
2019-01-02 17:44:34 +0100

    some data

--- src/qrTest.js
+++ src/qrTest.js
@@ -1,5 +1,5 @@
 var qrcode = require('qrcode-terminal');
 
-qrcode.generate('0x0D16730cc6bf84aAAe867CB6662e704D6e954a4C', function (str) { 
+qrcode.generate('dapp://0x0D16730cc6bf84aAAe867CB6662e704D6e954a4C', function (str) { 
     console.log(str);
 });
